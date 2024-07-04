d42bcad83bec3d0865f374f89ca7bffe9133c714
Jonas Jørgensen <lj.jonas@gmail.com>
2024-07-04 21:50:41 +0200

    added formatting of email, now we can add content to subject and body

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # weather_station
+
+sender_email = "insert sender email"
+
+password = "insert password"
+
+receiver_email = "insert_receiver_email1"
+receiver_email1 = "insert_receiver_email1"
+
+Go to this homepage to setup a gmail account to send automatic messages
+https://myaccount.google.com/apppasswords
--- main_mail.py
+++ main_mail.py
@@ -1,4 +1,7 @@
 import smtplib, ssl
+from email.mime.text import MIMEText
+from datetime import datetime
+
 from constants import sender_email, password, receiver_email, receiver_email1
 port = 587
 
@@ -8,18 +11,25 @@ password = password
 receiver_email = receiver_email
 
 
-message = "Subject: Hi there This This is a message from Python. \
-    Body: This is a test"
+text = "day 1: Hi there This This is a message from Python." + datetime.today().strftime('%Y-%m-%d')
+message = MIMEText(text, "plain")
+message["subject"] = "Grevinge winter weather updates"
+message["From"] = sender_email
+message["To"] = receiver_email
+
 
 context = ssl.create_default_context()
 
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()
-    server.starttls(context=context)
-    server.ehlo()
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
-    print("Email send successfully")
+try:
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()
+        server.starttls(context=context)
+        server.ehlo()
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        print("Email send successfully")
+except Exception as e:
+    print(f"Failed to send email: {e}")
 '''
 with smtplib.SMTP(smtp_server, port) as server:
     server.ehlo()
