a5fa58f70e6f036f8d904194a206471032e9fa05
Jonas Jørgensen <lj.jonas@gmail.com>
2024-07-04 22:02:14 +0200

    updated README.md file

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # weather_station
 
+## constants are formatted like this:
+
 sender_email = "insert sender email"
 
 password = "insert password"
@@ -7,5 +9,6 @@ password = "insert password"
 receiver_email = "insert_receiver_email1"
 receiver_email1 = "insert_receiver_email1"
 
-Go to this homepage to setup a gmail account to send automatic messages
+## Go to this homepage to setup a gmail account to send automatic messages
+
 https://myaccount.google.com/apppasswords
--- main.py
+++ main.py
@@ -13,12 +13,9 @@ driver.get('https://www.dmi.dk/lokation/show/DK/2621213/Grevinge/')
 a = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "coi-banner__accept")))
 a = driver.find_element(By.CLASS_NAME, "coi-banner__accept")
 a.click()
-#print(a.get_attribute('innerHTML'))
 
-
+# Wait for homepage to load
 wait = WebDriverWait(driver, 10)
-#//*[@id="0-content"]/div[2]/p[5]    //*[@id="1-accordionitem-day"]/div/p[3]/span
-#b = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, "//*[@id='0-accordionitem-day']/div/p[3]/span")))[0]
 
 for x in range(10):
     b = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, f"//*[@id='{x}-accordionitem-day']/div/p[3]/span")))[0]
